07dae700cc589f96c1c2fd5b548c2f99fb77db1d
Diego Henrique <diegohrp20@gmail.com>
2022-03-11 19:30:43 -0300

    Finishing project, building a blog web page with posts consumed from GraphCMS API data.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import { Provider } from 'urql'
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
 import { client, ssrCache } from '../lib/urql'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  if(pageProps.urqlState){
+  if (pageProps.urqlState) {
     ssrCache.restoreData(pageProps.urqlState);
   }
 
   return (
     <Provider value={client}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   )
 }
 
-export default MyApp
+export default MyApp;
