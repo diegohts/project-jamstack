a20ba4f11a7aae3c119f0bc26093821242f84693
Diego Henrique <diegohrp20@gmail.com>
2022-03-11 18:08:41 -0300

    Consuming requests from our client and exposing on the page.

--- src/lib/urql.ts
+++ src/lib/urql.ts
@@ -4,8 +4,8 @@ const isServerSide = typeof window === 'undefined';
 const ssrCache = ssrExchange({ isClient: !isServerSide });
 
 const client = createClient({
-    url: 'https://api-sa-east-1.graphcms.com/v2/cl0lk93ig64q801xt5g9n3m3a/master',
-    exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
-});
+  url: 'https://api-us-east-1.graphcms.com/v2/ckznjebnw0duj01yy3iy0e44x/master',
+  exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+})
 
 export { client, ssrCache };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,27 @@
+import { GetServerSideProps } from "next"
+import { usePageQuery, PageDocument } from "../generated/graphql"
+import { client, ssrCache } from "../lib/urql"
+
 export default function Home() {
+  const [{ data }] = usePageQuery({ 
+    variables: { 
+      slug: 'home' 
+    } 
+  })
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>{data?.page.title}</h1>
+    </>
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData()
+    }
+  }
+}
